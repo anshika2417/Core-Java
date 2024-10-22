0117e3b6cd41d7c8d3a507e30a0ef8f386745b0c
anshika2417 <anshikakumari571@gmail.com>
2024-10-22 07:56:56 +0530

    Do While Loop

---
+++ Loop/DoWhile.java
@@ -0,0 +1,12 @@
+class DoWhile{
+    public static void main(String []args){
+        
+        int i=5;
+        
+        do        // Runs the condition atleast once
+        {
+            System.out.println("Hi "+i);
+            i++;
+        }while(i<=4);
+    }
+}
