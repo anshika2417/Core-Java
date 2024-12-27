6dc7d479579eec2d2deae783fd67bc3ed57881ef
anshika2417 <anshikakumari571@gmail.com>
2024-12-27 14:35:15 +0530

    Rectangular Pattern

---
+++ Patterns/RectangularPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class RectangularPattern{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter Number of Rows: ");
+        int row=sc.nextInt();
+        System.out.print("Enter Number of Columns: ");
+        int col=sc.nextInt();
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
