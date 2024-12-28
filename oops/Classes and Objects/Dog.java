b1cb10bd7d11067a75bcf91999fffe99c55baf0c
anshika2417 <anshikakumari571@gmail.com>
2024-12-28 13:41:36 +0530

    Classes and Objects

---
+++ oops/Classes and Objects/Dog.java
@@ -0,0 +1,17 @@
+class Dog{
+     String name;
+     String color;
+     int age;
+
+     public void barking(){
+        System.out.println("Dog is Barking");
+     }
+
+     public void eating(){
+        System.out.println("Dog is Eating");
+     }
+
+     public void printColor(){
+        System.out.println(color);
+     }
+}
---
+++ oops/Classes and Objects/Main.java
@@ -0,0 +1,15 @@
+
+class Main{
+    public static void main(String[] args){
+        Dog tommy = new Dog();
+        tommy.color="Black";
+        tommy.barking();
+        tommy.printColor();
+
+
+        Dog dog1=new Dog();
+        dog1.eating();
+        dog1.color="White";
+        dog1.printColor();
+    }
+}
