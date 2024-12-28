b83489c825017bcec26db41dc8f2f9d99629dfff
anshika2417 <anshikakumari571@gmail.com>
2024-12-28 14:30:57 +0530

    Default Constructor

---
+++ oops/Constructor/Default.java
@@ -0,0 +1,13 @@
+class Default{
+    String name;
+    int age;
+
+    Default(){
+        System.out.println("This is Default Constructor");
+    }
+
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.print("AGe: "+age);
+    }
+}
---
+++ oops/Constructor/DefaultMain.java
@@ -0,0 +1,6 @@
+class DefaultMain{
+    public static void main(String[] args){
+        Default d=new Default();
+        d.display();
+    }
+}
