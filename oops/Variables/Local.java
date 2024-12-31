b7524d9573793d2bf29845adaac533c862cb5d9b
anshika2417 <anshikakumari571@gmail.com>
2024-12-31 20:11:25 +0530

    Local Variable

---
+++ oops/Variables/Local.java
@@ -0,0 +1,9 @@
+public class Local{
+    String color;
+    String name;
+
+    void print(){
+        int age=0;
+        System.out.println(age);
+    }
+}
---
+++ oops/Variables/LocalMain.java
@@ -0,0 +1,6 @@
+class LocalMain{
+    public static void main(String[] args){
+        Local loc=new Local();
+        loc.print();
+    }
+}
